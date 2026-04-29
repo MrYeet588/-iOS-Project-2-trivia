4f0c5e90a24da4ca7159472384cf7072ce4b8760
BT_C09_06 <mobileappdev@bths.edu>
2026-04-29 13:49:42 -0400

    idk if this works

--- Question.swift
+++ Question.swift
@@ -5,3 +5,17 @@
 //  Created by Student on 4/29/26.
 //
 
+struct Question{
+    var question: String,
+    
+    enum answers {
+    case answer1
+        
+    case answer2
+        
+    case answer3
+        
+    case answer4
+        
+    }
+}

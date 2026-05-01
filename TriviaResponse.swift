daf35dd927ee6028e19ee85247aa904ae0c9ec98
BT_C09_20 <mobileappdev@bths.edu>
2026-05-01 14:01:27 -0400

    moved question part 2

--- TriviaResponse.swift
+++ TriviaResponse.swift
@@ -8,3 +8,19 @@
 struct TriviaResponse: Codable {
     let results: [Question]
 }
+
+struct Question: Codable {
+    var type: String
+    var difficulty: String
+    var category: String
+    var question: String
+    var correct_answer: String
+    var incorrect_answers: [String]
+    
+    enum answers {
+        case answer1
+        case answer2
+        case answer3
+        case answer4
+    }
+}
